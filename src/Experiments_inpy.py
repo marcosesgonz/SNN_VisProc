edcb3f34519e55e31e6f500c6eb12204e4fec07d
Marcos Esquivel Gonzalez <121448114+marcosesgonz@users.noreply.github.com>
2023-07-10 16:14:14 +0100

    Incluying to models data augmentation possibility

--- src/Experiments_inpy.py
+++ src/Experiments_inpy.py
@@ -26,6 +26,7 @@ if __name__ == '__main__':
         dataset = data_dailyactions
     elif dataset == 'ActRec':
         dataset = data_actrec 
+    DatAug_prob = float(inputdata('Proportion of data augmentation(default = 0, recommeded = 0.5)', 0))
     timestep_ = int(inputdata('Time step (default = 16): ', 16))
     split_strat = inputdata('Split strategy (number(default) / time/ exp_decay ): ', 'number')
     tau_factor,scale_factor = None, None
@@ -47,5 +48,6 @@ if __name__ == '__main__':
                                     net_name=netname,
                                     run_id = runid_,
                                     factor_tau= tau_factor,
-                                    scale_factor= scale_factor)
+                                    scale_factor= scale_factor,
+                                    data_aug_prob = DatAug_prob) 
     
--- src/Laboratory.py
+++ src/Laboratory.py
@@ -10,6 +10,7 @@ from spikingjelly.datasets.dvs128_gesture import DVS128Gesture
 from torch.utils.tensorboard import SummaryWriter
 from sklearn.model_selection import train_test_split
 from models import myDVSGestureNet, mysew_resnet18
+from data_augmentation import EventMix
 import numpy as np
 import time
 import os
@@ -24,7 +25,7 @@ torch.backends.mps.deterministic = True
 
 data_dir = '/Users/marcosesquivelgonzalez/Desktop/MasterCDatos/TFM/data/DVS_Gesture_dataset'
 
-def loading_data(input_data,time_step = 16 ,splitmeth = 'number',tr_tst_split = True,tau_factor = 0.8,scale_factor = 50):
+def loading_data(input_data,time_step = 16 ,splitmeth = 'number',tr_tst_split = True,tau_factor = 0.8,scale_factor = 50, data_aug_prob = 0):
     relative_root = os.path.basename(input_data)
     if relative_root == 'DVS_Gesture_dataset':
         train_set = DVS128Gesture(root = input_data, train = True, data_type = 'frame', frames_number = time_step, 
@@ -51,10 +52,15 @@ def loading_data(input_data,time_step = 16 ,splitmeth = 'number',tr_tst_split = 
     
     num_classes = len(train_set.classes)
     size_xy = train_set.get_H_W()
+    if data_aug_prob != 0:
+        #OJO, EventMix da ya las etiquetas con one_hot encoding al tener que mixear etiquetas para aumentar datos.
+        train_set = EventMix(dataset=train_set, num_class = len(train_set.classes),num_mix = 1,
+                             beta = 1, prob = data_aug_prob, noise = 0.05, gaussian_n = 3) 
     if tr_tst_split:
         return train_set,test_set,num_classes,size_xy
     else: 
         return ConcatDataset([train_set,test_set]), num_classes, size_xy
+
 
 def load_net(net_name, n_classes, size_xy):
     if net_name == 'DVSG_net':
@@ -69,7 +75,7 @@ def load_net(net_name, n_classes, size_xy):
 
 
 
-def train_model(net, n_classes, tr_loader, optimizer, device, lr_scheduler):
+def train_model(net, n_classes, tr_loader, optimizer, device, lr_scheduler,data_augmented = False):
         net.train()
         train_loss = 0
         train_acc = 0
@@ -78,17 +84,39 @@ def train_model(net, n_classes, tr_loader, optimizer, device, lr_scheduler):
             optimizer.zero_grad()
             frame = frame.to(device)
             frame = frame.transpose(0, 1)  # [N, T, C, H, W] -> [T, N, C, H, W]  IMPORTANT, COULDN'T BE NECESSARY FOR SOME DATASETS
+
             label = label.to(device)
-            label_onehot = F.one_hot(label, n_classes).float()
+
+            if data_augmented:
+                label_onehot = label
+                num_batch_samples = len(label_onehot)      
+            else:
+                label_onehot = F.one_hot(label, n_classes).float()
+                num_batch_samples = label.numel()
 
             out_fr = net(frame).mean(0)
             loss = F.mse_loss(out_fr, label_onehot)
             loss.backward()
             optimizer.step()
 
-            train_samples += label.numel()
-            train_loss += loss.item() * label.numel()
-            train_acc += (out_fr.argmax(1) == label).float().sum().item()
+            train_samples += num_batch_samples
+            train_loss += loss.item() * num_batch_samples
+
+            if data_augmented:
+                max_labels, pred_indices = label.topk(k=2, dim=1)  # Índices de los 2 máximos en la dimensión 1
+                _, label_indices = out_fr.topk(k=2, dim=1)
+                correct = 0.
+                for i in range(num_batch_samples):
+                    if max_labels[i,0] == 1: #Si esto pasa, significa que la etiqueta de la instancia está bien definida con una etiqueta
+                        if pred_indices[i,0] == label_indices[i,0]:
+                            correct += 1.
+                    else:
+                        if torch.all(pred_indices[i,:] == pred_indices[i,:]):
+                            correct += 1.
+
+                train_acc += correct
+            else:
+                train_acc += (out_fr.argmax(1) == label).float().sum().item() #Con argmax en la dimensión 1(.argmax(1)) estoy deshaciendo el onehot para cada etiqueta del batch 
 
             functional.reset_net(net)           #DUDA ¿Por qué no se resetean los pesos después de cada instancia en lugar después de haber pasado todo el batch?
 
@@ -124,13 +152,14 @@ def test_model(net, n_classes,tst_loader,
 def execute_experiment_v2(T = 16,splitby = 'number',batch_size = 8, epochs = 30,
                         device = 'mps',lr = 0.1, inp_data= data_dir, 
                         net_name = 'DVSG_net',run_id = None, split_tr_tst = True,
-                        factor_tau = 0.8 , scale_factor = 50):
+                        factor_tau = 0.8 , scale_factor = 50, data_aug_prob = 0):
     
     relative_root = os.path.basename(inp_data)
     #Carga de datos en función del dataset que se vaya a usar
     train_set,test_set, nclasses_, sizexy = loading_data(input_data = inp_data,time_step = T,
                                                          splitmeth = splitby,tr_tst_split = split_tr_tst,
-                                                         tau_factor = factor_tau,scale_factor= scale_factor)
+                                                         tau_factor = factor_tau,scale_factor= scale_factor,
+                                                         data_aug_prob = data_aug_prob)
     train_size_,test_size_ = len(train_set),len(test_set)
     #Arquitectura de red que se va a usar, modo multipaso 'm' por defecto
     net = load_net(net_name = net_name, n_classes = nclasses_, size_xy = sizexy)
@@ -150,6 +179,7 @@ def execute_experiment_v2(T = 16,splitby = 'number',batch_size = 8, epochs = 30,
             batch_size=batch_size,
             learning_rate=lr,
             dataset=relative_root,
+            DatAug_probability = data_aug_prob,
             architecture=net_name)
         if splitby == 'exp_decay':
             hyperparameters['tau factor'] = factor_tau
@@ -210,7 +240,8 @@ def execute_experiment_v2(T = 16,splitby = 'number',batch_size = 8, epochs = 30,
         writer = SummaryWriter(out_dir, purge_step=start_epoch)
         for epoch in range(start_epoch, epochs):
             train_loss, train_acc = train_model(net=net, n_classes = nclasses_,tr_loader = train_data_loader,
-                                                optimizer = optimizer,device = device, lr_scheduler = lr_scheduler)
+                                                optimizer = optimizer,device = device, lr_scheduler = lr_scheduler,
+                                                data_augmented= data_aug_prob!=0)
             writer.add_scalar('train_loss', train_loss, epoch)
             writer.add_scalar('train_acc', train_acc, epoch)
 
